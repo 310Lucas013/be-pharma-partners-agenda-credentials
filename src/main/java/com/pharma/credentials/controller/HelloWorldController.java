4d7042c5e9c2bcc1633df29e511ca81b317f643b
JayvHelderen <jayvanhelderen@hotmail.com>
2021-03-18 15:14:12 +0100

    Feature not working yet

--- src/main/java/com/pharma/credentials/controller/HelloWorldController.java
+++ src/main/java/com/pharma/credentials/controller/HelloWorldController.java
@@ -1,15 +1,31 @@
 package com.pharma.credentials.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.pharma.credentials.models.UserEmail;
+import com.pharma.credentials.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
 
 @RestController
 @CrossOrigin()
 public class HelloWorldController {
+    @Autowired
+    EmailService emailService;
+
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public String getEmployees() {
         return "Welcome!";
+    }
+
+    @RequestMapping(value = "/email", method = RequestMethod.POST)
+    public ResponseEntity<?> sendEmail(@RequestBody UserEmail user) throws Exception {
+        String twoFaCode = String.valueOf(new Random().nextInt(9999) + 1000);
+
+        emailService.sendEmail(user.getEmail(), twoFaCode);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
